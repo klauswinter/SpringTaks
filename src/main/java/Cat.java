8e527f6218b42497f311761508d60a3212edceb4
Danil <korotkin321@gmail.com>
2021-11-09 11:56:53 +0300

    ÐВыполMade the changes written in the task

---
+++ src/main/java/Cat.java
@@ -0,0 +1,42 @@
+public class Cat {
+    private String name;
+    private String message;
+    private int age;
+    private int paws;
+
+    public Cat() {
+
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setPaws(int paws) {
+        this.paws = paws;
+    }
+
+    public int getPaws() {
+        return paws;
+    }
+}
